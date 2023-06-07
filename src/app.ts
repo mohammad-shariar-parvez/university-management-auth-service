838a03d2a1ce21c4c468ea0db1a798d31429b128
Piaowei <msptonmoy@gmail.com>
2023-06-07 22:47:03 +0600

    Setup eslint prettier husky lint-staged

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
-import express, { Application, Request, Response } from 'express';
-import cors from 'cors';
-const app: Application = express();
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 
+//parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+//Testing
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
+  res.send('Hello World!')
+})
 
-export default app;
+export default app
 // getting-started.js
 // const mongoose = require('mongoose');
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-import mongoose from 'mongoose';
-import app from './app';
-import config from './config';
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function bootstrap() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log(`Database connected Successfully`);
+    await mongoose.connect(config.database_url as string)
+    console.log(`Database connected Successfully`)
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
-    });
+      console.log(`Example app listening on port ${config.port}`)
+    })
   } catch (error) {
-    console.log('Failed to connect db', error);
+    console.log('Failed to connect db', error)
   }
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
-bootstrap();
+bootstrap()
