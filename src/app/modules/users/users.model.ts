0ae3b99a626578e478bc4cd6286476ca3e6d53df
Piaowei <msptonmoy@gmail.com>
2023-06-09 17:02:32 +0600

    [DONE] Improve lintin setup

--- src/app/modules/users/users.model.ts
+++ src/app/modules/users/users.model.ts
@@ -1,10 +1,8 @@
 import { Model, Schema, model } from 'mongoose'
 import { IUser } from './users.interface'
 
-// Create a new Model type that knows about upcimming any methods...
 type UserModel = Model<IUser, object>
 
-// 2. Create a Schema corresponding to the document interface.
 export const userSchema = new Schema<IUser>(
   {
     id: {
